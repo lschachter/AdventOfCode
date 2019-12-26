155150cb470c379595293805af752f0ae08f24d8
lschachter <lillieschachter@gmail.com>
2019-12-26 03:02:17 -0800

    Add day 3 challenge 2

--- 2019/Day_03/wireIntersection.py
+++ 2019/Day_03/wireIntersection.py
@@ -53,13 +53,59 @@ def getAxisData(move):
     yStep = MOVES[direction][1] if not x else 1
     return x, y, xStep, yStep
 
+
+def countPath(path):
+    origin = (0,0)
+    currStart = list(origin)
+    steps = 0
+    pathPoints = {}
+    for move in path:
+        x, y, xStep, yStep = getAxisData(move)
+        for i in range(currStart[0] + xStep, currStart[0] + x + xStep, xStep):
+            current = tuple([i, currStart[1]])
+            steps += 1
+            pathPoints[current] = steps
+        for i in range(currStart[1] + yStep, currStart[1] + y + yStep, yStep):
+            current = tuple([currStart[0], i])
+            steps += 1
+            pathPoints[current] = steps
+
+        currStart[0] += x
+        currStart[1] += y
+    return pathPoints
+
+def findSmallestSum(pathPoints, path2):
+    minIntersection = math.inf
+    currStart = [0,0]
+    steps = 0
+    for move in path2:
+        x, y, xStep, yStep = getAxisData(move)
+        for i in range(currStart[0] + xStep, currStart[0] + x + xStep, xStep):
+            current = tuple([i, currStart[1]])
+            steps += 1
+            if current in pathPoints:
+                minIntersection = min(minIntersection, pathPoints[current] + steps)
+        for i in range(currStart[1] + yStep, currStart[1] + y + yStep, yStep):
+            current = tuple([currStart[0], i])
+            steps += 1
+            if current in pathPoints:
+                minIntersection = min(minIntersection, pathPoints[current] + steps)
+        
+        currStart[0] += x
+        currStart[1] += y
+    return minIntersection
+
 def main():
     paths = open("wire_paths.txt", "r").readlines()
     path1 = paths[0].split(',')
     path2 = paths[1].split(',')
-    pathPoints = buildPath(path1)
+    pathPoints = countPath(path1)
+
+    # pathPoints = buildPath(path1)
+    # minIntersection = findIntersection(pathPoints, path2)
+    # print(minIntersection)
     
-    minIntersection = findIntersection(pathPoints, path2)
+    minIntersection = findSmallestSum(pathPoints, path2)
     print(minIntersection)
 
 main()
