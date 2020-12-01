c3df5df65e5574b4dde712a24374f8fc455527d7
Lillie Schachter <lillieschachter@gmail.com>
2020-11-30 22:38:16 -0800

    Add 2020 day 1 part 2

--- 2020/day_1/report_repair.py
+++ 2020/day_1/report_repair.py
@@ -6,11 +6,9 @@
 
 def build_expense_list():
     report = open('expense_report.txt', 'r').read()
-    return map(int, report.split('\n'))
+    return list(map(int, report.split('\n')))
 
-
-def sum_two(k):
-    expenses = build_expense_list()
+def sum_two(k, expenses):
     expenses = sorted(expenses)
 
     p1 = 0
@@ -18,7 +16,6 @@ def sum_two(k):
 
     while p1 != p2:
         if expenses[p1] + expenses[p2] == k:
-            print(expenses[p1], expenses[p2])
             return expenses[p1] * expenses[p2]
         if expenses[p1] + expenses[p2] < k:
             p1 += 1
@@ -27,4 +24,28 @@ def sum_two(k):
 
     return 0
 
-print(sum_two(2020))
+# same idea but gonna be much slower-- you have to check each pair against a number between them,
+# so you have to do the same process as above, but n times (n being the number of values in the list)
+def sum_three(k, expenses):
+    expenses = sorted(expenses)
+    num_expenses = len(expenses)
+
+    for i in range(num_expenses):
+        p1 = 0
+        p2 = i + 1
+        p3 = len(expenses) - 1
+
+        while p1 < p3:
+            if expenses[p1] + expenses[p2] + expenses[p3] == k:
+                return expenses[p1] * expenses[p2] * expenses[p3]
+            if expenses[p1] + expenses[p2] + expenses[p3] < k:
+                p1 += 1
+            else:
+                p3 -= 1
+
+    return 0
+
+
+expenses = build_expense_list()
+# print(sum_two(2020, expenses))
+print(sum_three(2020, expenses))
