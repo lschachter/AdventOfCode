14b218d2eb9d076eae93f17497d57a2299ce2bb8
Lillie Schachter <lillieschachter@gmail.com>
2024-12-24 14:10:06 -0800

    use typing to get that startswith yellow baybeeeee

--- 2024/day24/boolean_logic.py
+++ 2024/day24/boolean_logic.py
@@ -11,18 +11,18 @@ gates: Dict[str, str] = dict(map(lambda gate: gate.split(": "), gates))
 rules: Dict[str, List[str]] = [rule.split() for rule in rules]
 
 # rules by result: result: gate1, gate2, operator
-rules = {rule[4]: [rule[0], rule[2], rule[1]] for rule in rules}
+rules: Dict[str, List[str]] = {rule[4]: [rule[0], rule[2], rule[1]] for rule in rules}
 
 
-def build_number(gates):
+def build_number(gates: dict) -> int:
     for rule, vals in rules.items():
         gates[rule] = get_gate(*vals)
 
-    gates = list(gates.items())
+    gates: List[str] = list(gates.items())
     gates.sort(key=lambda pair: pair[0])
 
-    num = []
-    i = -1
+    num: list = []
+    i: int = -1
     while gates[i][0].startswith("z"):
         num.append(gates[i][1])
         i -= 1
@@ -30,9 +30,9 @@ def build_number(gates):
     return int("".join(num), 2)
 
 
-def get_gate(gate_1, gate_2, operator):
-    right = gates.get(gate_1) or get_gate(*rules[gate_1])
-    left = gates.get(gate_2) or get_gate(*rules[gate_2])
+def get_gate(gate_1: str, gate_2: str, operator: str) -> str:
+    right: str = gates.get(gate_1) or get_gate(*rules[gate_1])
+    left: str = gates.get(gate_2) or get_gate(*rules[gate_2])
 
     gates[gate_1] = right
     gates[gate_2] = left
